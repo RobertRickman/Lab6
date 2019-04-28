029fe123c0840e53fe4fa119faec970b595cc9d6
RobertRickman <48113308+RobertRickman@users.noreply.github.com>
2019-04-27 23:50:40 -0700

    new addition

---
+++ Lab6/Lab6/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.BookIDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BookNameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BookAuthorDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BookAdditionYearDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BooksBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Lab6DataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Lab6DataSet = New Lab6.Lab6DataSet()
+        Me.btnBooks = New System.Windows.Forms.Button()
+        Me.btnPub = New System.Windows.Forms.Button()
+        Me.btnYear = New System.Windows.Forms.Button()
+        Me.txtPub = New System.Windows.Forms.TextBox()
+        Me.txtYear = New System.Windows.Forms.TextBox()
+        Me.OleDbConnection1 = New System.Data.OleDb.OleDbConnection()
+        Me.OleDbSelectCommand1 = New System.Data.OleDb.OleDbCommand()
+        Me.OleDbInsertCommand1 = New System.Data.OleDb.OleDbCommand()
+        Me.OleDbUpdateCommand1 = New System.Data.OleDb.OleDbCommand()
+        Me.OleDbDeleteCommand1 = New System.Data.OleDb.OleDbCommand()
+        Me.OleDbDataAdapter1 = New System.Data.OleDb.OleDbDataAdapter()
+        Me.BooksTableAdapter = New Lab6.Lab6DataSetTableAdapters.BooksTableAdapter()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BooksBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Lab6DataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Lab6DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.BookIDDataGridViewTextBoxColumn, Me.BookNameDataGridViewTextBoxColumn, Me.BookAuthorDataGridViewTextBoxColumn, Me.BookAdditionYearDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.BooksBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 22)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(445, 320)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'BookIDDataGridViewTextBoxColumn
+        '
+        Me.BookIDDataGridViewTextBoxColumn.DataPropertyName = "BookID"
+        Me.BookIDDataGridViewTextBoxColumn.HeaderText = "BookID"
+        Me.BookIDDataGridViewTextBoxColumn.Name = "BookIDDataGridViewTextBoxColumn"
+        '
+        'BookNameDataGridViewTextBoxColumn
+        '
+        Me.BookNameDataGridViewTextBoxColumn.DataPropertyName = "BookName"
+        Me.BookNameDataGridViewTextBoxColumn.HeaderText = "BookName"
+        Me.BookNameDataGridViewTextBoxColumn.Name = "BookNameDataGridViewTextBoxColumn"
+        '
+        'BookAuthorDataGridViewTextBoxColumn
+        '
+        Me.BookAuthorDataGridViewTextBoxColumn.DataPropertyName = "BookAuthor"
+        Me.BookAuthorDataGridViewTextBoxColumn.HeaderText = "BookAuthor"
+        Me.BookAuthorDataGridViewTextBoxColumn.Name = "BookAuthorDataGridViewTextBoxColumn"
+        '
+        'BookAdditionYearDataGridViewTextBoxColumn
+        '
+        Me.BookAdditionYearDataGridViewTextBoxColumn.DataPropertyName = "BookAdditionYear"
+        Me.BookAdditionYearDataGridViewTextBoxColumn.HeaderText = "BookAdditionYear"
+        Me.BookAdditionYearDataGridViewTextBoxColumn.Name = "BookAdditionYearDataGridViewTextBoxColumn"
+        '
+        'BooksBindingSource
+        '
+        Me.BooksBindingSource.DataMember = "Books"
+        Me.BooksBindingSource.DataSource = Me.Lab6DataSetBindingSource
+        '
+        'Lab6DataSetBindingSource
+        '
+        Me.Lab6DataSetBindingSource.DataSource = Me.Lab6DataSet
+        Me.Lab6DataSetBindingSource.Position = 0
+        '
+        'Lab6DataSet
+        '
+        Me.Lab6DataSet.DataSetName = "Lab6DataSet"
+        Me.Lab6DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'btnBooks
+        '
+        Me.btnBooks.Location = New System.Drawing.Point(48, 404)
+        Me.btnBooks.Name = "btnBooks"
+        Me.btnBooks.Size = New System.Drawing.Size(75, 23)
+        Me.btnBooks.TabIndex = 5
+        Me.btnBooks.Text = "All Books"
+        Me.btnBooks.UseVisualStyleBackColor = True
+        '
+        'btnPub
+        '
+        Me.btnPub.Location = New System.Drawing.Point(167, 404)
+        Me.btnPub.Name = "btnPub"
+        Me.btnPub.Size = New System.Drawing.Size(100, 23)
+        Me.btnPub.TabIndex = 6
+        Me.btnPub.Text = "Publisher"
+        Me.btnPub.UseVisualStyleBackColor = True
+        '
+        'btnYear
+        '
+        Me.btnYear.Location = New System.Drawing.Point(313, 404)
+        Me.btnYear.Name = "btnYear"
+        Me.btnYear.Size = New System.Drawing.Size(100, 23)
+        Me.btnYear.TabIndex = 7
+        Me.btnYear.Text = "Book Year"
+        Me.btnYear.UseVisualStyleBackColor = True
+        '
+        'txtPub
+        '
+        Me.txtPub.Location = New System.Drawing.Point(167, 373)
+        Me.txtPub.Name = "txtPub"
+        Me.txtPub.Size = New System.Drawing.Size(100, 20)
+        Me.txtPub.TabIndex = 8
+        '
+        'txtYear
+        '
+        Me.txtYear.Location = New System.Drawing.Point(313, 373)
+        Me.txtYear.Name = "txtYear"
+        Me.txtYear.Size = New System.Drawing.Size(100, 20)
+        Me.txtYear.TabIndex = 9
+        '
+        'OleDbConnection1
+        '
+        Me.OleDbConnection1.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\Lab61.mdb"
+        '
+        'OleDbSelectCommand1
+        '
+        Me.OleDbSelectCommand1.CommandText = "SELECT        Books.*" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "FROM            Books"
+        '
+        'OleDbInsertCommand1
+        '
+        Me.OleDbInsertCommand1.CommandText = "INSERT INTO `Books` (`BookID`, `BookName`, `BookAuthor`, `BookAdditionYear`) VALU" &
+    "ES (?, ?, ?, ?)"
+        Me.OleDbInsertCommand1.Parameters.AddRange(New System.Data.OleDb.OleDbParameter() {New System.Data.OleDb.OleDbParameter("BookID", System.Data.OleDb.OleDbType.[Integer], 0, "BookID"), New System.Data.OleDb.OleDbParameter("BookName", System.Data.OleDb.OleDbType.VarWChar, 0, "BookName"), New System.Data.OleDb.OleDbParameter("BookAuthor", System.Data.OleDb.OleDbType.VarWChar, 0, "BookAuthor"), New System.Data.OleDb.OleDbParameter("BookAdditionYear", System.Data.OleDb.OleDbType.VarWChar, 0, "BookAdditionYear")})
+        '
+        'OleDbUpdateCommand1
+        '
+        Me.OleDbUpdateCommand1.CommandText = resources.GetString("OleDbUpdateCommand1.CommandText")
+        Me.OleDbUpdateCommand1.Parameters.AddRange(New System.Data.OleDb.OleDbParameter() {New System.Data.OleDb.OleDbParameter("BookID", System.Data.OleDb.OleDbType.[Integer], 0, "BookID"), New System.Data.OleDb.OleDbParameter("BookName", System.Data.OleDb.OleDbType.VarWChar, 0, "BookName"), New System.Data.OleDb.OleDbParameter("BookAuthor", System.Data.OleDb.OleDbType.VarWChar, 0, "BookAuthor"), New System.Data.OleDb.OleDbParameter("BookAdditionYear", System.Data.OleDb.OleDbType.VarWChar, 0, "BookAdditionYear"), New System.Data.OleDb.OleDbParameter("Original_BookID", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "BookID", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_BookName", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "BookName", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_BookName", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "BookName", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_BookAuthor", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "BookAuthor", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_BookAuthor", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "BookAuthor", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_BookAdditionYear", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "BookAdditionYear", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_BookAdditionYear", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "BookAdditionYear", System.Data.DataRowVersion.Original, Nothing)})
+        '
+        'OleDbDeleteCommand1
+        '
+        Me.OleDbDeleteCommand1.CommandText = resources.GetString("OleDbDeleteCommand1.CommandText")
+        Me.OleDbDeleteCommand1.Parameters.AddRange(New System.Data.OleDb.OleDbParameter() {New System.Data.OleDb.OleDbParameter("Original_BookID", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "BookID", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_BookName", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "BookName", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_BookName", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "BookName", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_BookAuthor", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "BookAuthor", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_BookAuthor", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "BookAuthor", System.Data.DataRowVersion.Original, Nothing), New System.Data.OleDb.OleDbParameter("IsNull_BookAdditionYear", System.Data.OleDb.OleDbType.[Integer], 0, System.Data.ParameterDirection.Input, CType(0, Byte), CType(0, Byte), "BookAdditionYear", System.Data.DataRowVersion.Original, True, Nothing), New System.Data.OleDb.OleDbParameter("Original_BookAdditionYear", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, False, CType(0, Byte), CType(0, Byte), "BookAdditionYear", System.Data.DataRowVersion.Original, Nothing)})
+        '
+        'OleDbDataAdapter1
+        '
+        Me.OleDbDataAdapter1.DeleteCommand = Me.OleDbDeleteCommand1
+        Me.OleDbDataAdapter1.InsertCommand = Me.OleDbInsertCommand1
+        Me.OleDbDataAdapter1.SelectCommand = Me.OleDbSelectCommand1
+        Me.OleDbDataAdapter1.TableMappings.AddRange(New System.Data.Common.DataTableMapping() {New System.Data.Common.DataTableMapping("Table", "Books", New System.Data.Common.DataColumnMapping() {New System.Data.Common.DataColumnMapping("BookID", "BookID"), New System.Data.Common.DataColumnMapping("BookName", "BookName"), New System.Data.Common.DataColumnMapping("BookAuthor", "BookAuthor"), New System.Data.Common.DataColumnMapping("BookAdditionYear", "BookAdditionYear")})})
+        Me.OleDbDataAdapter1.UpdateCommand = Me.OleDbUpdateCommand1
+        '
+        'BooksTableAdapter
+        '
+        Me.BooksTableAdapter.ClearBeforeFill = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(599, 450)
+        Me.Controls.Add(Me.txtYear)
+        Me.Controls.Add(Me.txtPub)
+        Me.Controls.Add(Me.btnYear)
+        Me.Controls.Add(Me.btnPub)
+        Me.Controls.Add(Me.btnBooks)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BooksBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Lab6DataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Lab6DataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnBooks As Button
+    Friend WithEvents btnPub As Button
+    Friend WithEvents btnYear As Button
+    Friend WithEvents txtPub As TextBox
+    Friend WithEvents txtYear As TextBox
+    Friend WithEvents OleDbConnection1 As OleDb.OleDbConnection
+    Friend WithEvents OleDbSelectCommand1 As OleDb.OleDbCommand
+    Friend WithEvents OleDbInsertCommand1 As OleDb.OleDbCommand
+    Friend WithEvents OleDbUpdateCommand1 As OleDb.OleDbCommand
+    Friend WithEvents OleDbDeleteCommand1 As OleDb.OleDbCommand
+    Friend WithEvents OleDbDataAdapter1 As OleDb.OleDbDataAdapter
+    Friend WithEvents Lab6DataSetBindingSource As BindingSource
+    Friend WithEvents Lab6DataSet As Lab6DataSet
+    Friend WithEvents BooksBindingSource As BindingSource
+    Friend WithEvents BooksTableAdapter As Lab6DataSetTableAdapters.BooksTableAdapter
+    Friend WithEvents BookIDDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents BookNameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents BookAuthorDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents BookAdditionYearDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+End Class
---
+++ Lab6/Lab6/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FindAll()
+    End Sub
+
+    Private Sub btnPub_Click(sender As Object, e As EventArgs) Handles btnPub.Click
+        fromPubName()
+    End Sub
+    Private Sub btnYear_Click(sender As Object, e As EventArgs) Handles btnYear.Click
+        fromYear()
+    End Sub
+    Private Sub FindAll()
+        Dim SQlStatement As String = "SELECT Book.BookName, Book.BookAuthor, Book.BookEditionYear FROM Books;"
+
+        Dim connectionString As String = "Provider = Microsoft.Jet.OLEDB.4.0;" & "Data Source = Lab6.mdb"
+        Dim dtProperties As New DataTable()
+        Dim dbDataAdapter As OleDbDataAdapter
+        dbDataAdapter = New OleDbDataAdapter(SQlStatement, connectionString)
+        dbDataAdapter.Fill(dtProperties)
+        DataGridView1.DataSource = dtProperties
+    End Sub
+    Private Sub fromPubName()
+        Dim SQlStatement As String = "SELECT Book.BookName, Book.BookAuthor, Book.BookEditionYear
+                                      FROM Publisher INNER JOIN Book ON Publisher.PubID = Book.PubID
+                                      WHERE (((Publisher.PubName)='" + txtPub.Text + "'));"
+
+        Dim connectionString As String = "Provider = Microsoft.et.OLEDB.4.0;" & "Data Source = Lab6.mdb"
+        Dim dtProperties As New DataTable()
+        Dim dbDataAdapter As OleDbDataAdapter
+        dbDataAdapter = New OleDbDataAdapter(SQlStatement, connectionString)
+        dbDataAdapter.Fill(dtProperties)
+        DataGridView1.DataSource = dtProperties
+    End Sub
+
+    Private Sub fromYear()
+        Dim SQlStatement As String = "SELECT Book.BookName, Book.BookAuthor, Book.BookEditionYear, Publisher.PubName
+                                      FROM Book INNER JOIN Publisher ON Book.PubID = Publisher.PubID
+                                      WHERE (((Book.BookEditionYear)='" + txtYear.Text + "'));"
+
+        Dim connectionString As String = "Provider = Microsoft.et.OLEDB.4.0;" & "Data Source = Lab6.mdb"
+        Dim dtProperties As New DataTable()
+        Dim dbDataAdapter As OleDbDataAdapter
+        dbDataAdapter = New OleDbDataAdapter(SQlStatement, connectionString)
+        dbDataAdapter.Fill(dtProperties)
+        DataGridView1.DataSource = dtProperties
+    End Sub
+
+End Class
